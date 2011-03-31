46dc02b41c5c8c7363dd083db824ad4f8f22e3b3
Oscar Vestlie <oscar.vestlie@atex.com>
2011-03-31 22:22:20 +0200

    added google analytics

---
+++ js/ga.js
@@ -0,0 +1,26 @@
+// Init Google Analytics
+var _gaq = _gaq || [];
+
+if (window.location.host === 'bild.veslife.se') {
+    _gaq.push(['_setAccount', 'UA-22429777-1']);
+    _gaq.push(['_trackPageview']);
+    
+    (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+}
+
+function trackEvent(category, action, label, value) {
+    var data = ['_trackEvent', category, action];
+
+    if (label) {
+        data.push(label);
+    }
+    if (value) {
+        data.push(value);
+    }
+
+    _gaq.push(data);
+}
